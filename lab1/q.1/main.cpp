14fe6a4201f1abe6b651dcc0290dc7e83290abd0
Muhammad Zaki <zakibhatti3015@gmail.com>
2023-09-19 16:55:17 +0500

    Eligible for Vaccination

--- lab1/q.1/main.cpp
+++ lab1/q.1/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Define a struct for PakistaniCitizen
 struct PakistaniCitizen {
   string name; // data member for name
-  int age; // data member for age
+  int age;     // data member for age
   char gender; // data member for gender
 };
 
@@ -23,17 +23,26 @@ bool isCovidVaccinated(PakistaniCitizen c) {
 int main() {
   // Create an object of PakistaniCitizen
   PakistaniCitizen p;
-  // Assign values to the data members
-  p.name = "Ali";
-  p.age = 15;
-  p.gender = 'M';
+
+  // Input citizen's information from the user
+  cout << "Enter the citizen's name: ";
+  cin >> p.name;
+
+  cout << "Enter the citizen's age: ";
+  cin >> p.age;
+
+  cout << "Enter the citizen's gender (M/F): ";
+  cin >> p.gender;
+
   // Call the function isCovidVaccinated with the object as an argument
   bool result = isCovidVaccinated(p);
+
   // Print the result based on the return value of the function
   if (result == true) {
     cout << "Citizen is Eligible for Vaccination" << endl;
   } else {
     cout << "Citizen is Not Eligible for Vaccination" << endl;
   }
+
   return 0;
 }
